47f05e21542246407fef0f2242c12246854e1e9f
Joel Grosjean <grosjean@sheridancollege.ca>
2017-03-28 14:07:48 -0400

    Some code clean up and added another role and user

--- prog35142.week11.security/prog35142.week11.security/Migrations/Configuration.cs
+++ prog35142.week11.security/prog35142.week11.security/Migrations/Configuration.cs
@@ -17,23 +17,38 @@ namespace prog35142.week11.security.Migrations
 
         protected override void Seed(prog35142.week11.security.Models.ApplicationDbContext context)
         {
+            var store = new RoleStore<IdentityRole>(context);
+            var manager = new RoleManager<IdentityRole>(store);
+
             if (!context.Roles.Any(r => r.Name == "Admin"))
             {
-                var store = new RoleStore<IdentityRole>(context);
-                var manager = new RoleManager<IdentityRole>(store);
                 var role = new IdentityRole { Name = "Admin" };
-
                 manager.Create(role);
             }
 
+            if (!context.Roles.Any(r => r.Name == "Staff"))
+            {
+                var role = new IdentityRole { Name = "Staff" };
+                manager.Create(role);
+            }
+
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
             //  This method will be called after migrating to the latest version.
             if (!(context.Users.Any(u => u.UserName == "student@sheridancollege.ca")))
             {
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
                 var userToInsert = new ApplicationUser { UserName = "student@sheridancollege.ca", PhoneNumber = "9055551234" };
                 userManager.Create(userToInsert, "student20!4");
                 userManager.AddToRole(userToInsert.Id, "Admin");
+            }
+
+            if (!(context.Users.Any(u => u.UserName == "staff@sheridancollege.ca")))
+            {
+                
+                var userToInsert = new ApplicationUser { UserName = "staff@sheridancollege.ca", PhoneNumber = "4165557894" };
+                userManager.Create(userToInsert, "password!");
+                userManager.AddToRole(userToInsert.Id, "Staff");
             }
         }
     }
