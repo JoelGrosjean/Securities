7bcfaff0ed6cd43288f8dd4188d09603bbd5169e
Joel Grosjean <grosjean@sheridancollege.ca>
2017-03-28 13:34:54 -0400

    Code from powerpoints was added to create the admin role and adding users. This only creates the information, it doesnt secure the site

--- prog35142.week11.security/prog35142.week11.security/Migrations/Configuration.cs
+++ prog35142.week11.security/prog35142.week11.security/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace prog35142.week11.security.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,26 @@ namespace prog35142.week11.security.Migrations
 
         protected override void Seed(prog35142.week11.security.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                manager.Create(role);
+            }
+
+            //  This method will be called after migrating to the latest version.
+            if (!(context.Users.Any(u => u.UserName == "student@sheridancollege.ca")))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userToInsert = new ApplicationUser { UserName = "student@sheridancollege.ca", PhoneNumber = "9055551234" };
+                userManager.Create(userToInsert, "student20!4");
+                userManager.AddToRole(userToInsert.Id, "Admin");
+            }
+
+
         }
     }
 }
