ec20f3499e32f9d7615d28599d1960bf65cdd5ea
Joel Grosjean <grosjean@sheridancollege.ca>
2017-03-28 13:09:24 -0400

    Initial Commit

---
+++ prog35142.week11.security/prog35142.week11.security/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="prog35142.week11.security.MvcApplication" Language="C#" %>
---
+++ prog35142.week11.security/prog35142.week11.security/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prog35142.week11.security.Startup))]
+namespace prog35142.week11.security
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
